e7bf8e08151c526a8133d6bcc74a0385b9186d2f
Zakzacho <jaafardarim@gmail.com>
2026-01-02 22:22:46 +0100

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-index.js
+import http from "http";
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Watch-VIP backend is running");
+}).listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
